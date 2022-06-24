204db826ae03a14d2d7afbdd35ade2e8aa619829
Cristiano <cristianovelkan@gmail.com>
2022-06-24 08:51:00 -0300

    feat: navigation config

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#9EF8EE",
+    primary100: "#00E3CC",
+    primary200: "#44E3D3",
+    primary400: "#22BABB",
+    primary500: "#32A89C",
+    primary700: "#348888",
+    primary800: "#00635A",
+    accent500: "#C4EEF2",
+    error50: "#fcc4e4",
+    error500: "#F24405",
+    gray500: "#39324a",
+    gray700: "#221c30",
+  },
+};
