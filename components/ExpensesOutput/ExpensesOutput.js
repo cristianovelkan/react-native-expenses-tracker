7b00b4ce41f7af5c43a983c18c29de12c3571e0b
Cristiano <cristianovelkan @gmail.com>
2022-06-30 09:05:53 -0300

    feat: expenses context provider

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,38 +3,11 @@ import { GlobalStyles } from "../../constants/styles";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "ex1",
-    description: "Rent",
-    amount: 950,
-    date: new Date("2022-06-28"),
-  },
-  {
-    id: "ex2",
-    description: "Course",
-    amount: 50,
-    date: new Date("2022-06-28"),
-  },
-  {
-    id: "ex3",
-    description: "Bananas",
-    amount: 5,
-    date: new Date("2022-06-25"),
-  },
-  {
-    id: "ex4",
-    description: "Eletric Bill",
-    amount: 85,
-    date: new Date("2022-06-30"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 }
--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,17 +1,15 @@
-import { StyleSheet, Text } from "react-native";
+import { useContext } from "react";
+import { StyleSheet } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
 
 function AllExpenses() {
-  return <ExpensesOutput expensesPeriod="Total" />;
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+    <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" />
+  );
 }
 
 export default AllExpenses;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create();
